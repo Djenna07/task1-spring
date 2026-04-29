21d7054d9d58a64e5c0940dd9922b0b4f33913c2
Djenna07 <dchebil1@stu.vistula.edu.pl>
2026-04-29 08:09:43 +0200

    First Spring Boot application - Task 1

---
+++ task1-spring/task1-spring/src/main/java/com/djenna/task1_spring/HelloController.java
@@ -0,0 +1,25 @@
+package com.djenna.task1_spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello! This is my first Spring controller.";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+            Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ task1-spring/task1-spring/src/test/java/com/djenna/task1_spring/Task1SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djenna.task1_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
